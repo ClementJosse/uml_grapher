3b504ad3d3c10e117cae2f891ecb6e30313b1514
JOSSE Clément <josse@et.esiea.fr>
2022-10-28 18:43:37 +0200

    ajout de 2 tests pour ameliorer le coverage

--- src/test/java/fr/lernejo/UmlGraphTests.java
+++ src/test/java/fr/lernejo/UmlGraphTests.java
@@ -20,9 +20,48 @@ public class UmlGraphTests {
                 <<interface>>
             }
             """);
+
+    }
+
+    @Test
+    void no_class_no_interface() {
+
+        Class[] classes = new Class[]{};
+        UmlGraph graph = new UmlGraph(classes);
+
+        String output = graph.as(GraphType.Mermaid);
+
+        Assertions.assertThat(output).isEqualTo("""
+            classDiagram""");
+
+    }
+
+    @Test
+    void multiple_interfaces_with_no_relation() {
+
+        Class[] classes = new Class[]{Machin.class, Truc.class};
+        UmlGraph graph = new UmlGraph(classes);
+
+        String output = graph.as(GraphType.Mermaid);
+
+        Assertions.assertThat(output).isEqualTo("""
+            classDiagram
+            class Machin {
+                <<interface>>
+            }
+            
+            class Truc {
+                <<interface>>
+            }
+            """);
+
     }
 
     interface Machin {
     }
+
+    interface Truc{
+    }
+
 
 }
